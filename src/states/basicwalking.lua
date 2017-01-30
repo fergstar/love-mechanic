b1fa3e1ee231f1cb064dabecaf4572b46f358430
Guy Fergusson <fergs@iinet.net.au>
2017-01-30 22:17:02 +0800

    moved basic walking to state, remove old mechanics, add quit option

--- src/states/basicwalking.lua
+++ src/states/basicwalking.lua
@@ -1,16 +1,54 @@
 local basicwalking = {}
+local player = {}
+local ground = {}
+
+-- define movement constants
+local MAX_SPEED = 500 -- pixels/second
 
 function basicwalking:enter()
 
+  -- Set stage background to something sky colored
+  love.graphics.setBackgroundColor(131, 192, 240)
+
+  width = love.graphics.getWidth()	-- get width of screen
+  height = love.graphics.getHeight()	-- height, too.
+
+  -- Create a player sprite.
+  player.img = love.graphics.newImage('assets/player.png')  
+  player.x = width / 2   -- This sets the player at the middle of the screen based on the width of the game window. 
+  player.y = height / 2  -- This sets the player at the middle of the screen based on the height of the game window. 
+ 
+  -- Create some ground for the player to walk on
+  ground.img = love.graphics.newImage('assets/ground.png')
+  ground.img:setWrap('repeat','clamp') 
+  ground.imgFrame = love.graphics.newQuad(0, 0, width, height, ground.img:getWidth(), ground.img:getHeight())	
+  
 end
 
 function basicwalking:draw()
-  love.graphics.setColor(255, 255, 255)
-  love.graphics.print("hello basicwalking", 200, 200)
+  
+    -- This draws the player.
+	love.graphics.draw(player.img, player.x, player.y, 0, 1, 1, 0, 32)
+  
+  -- This draws the ground.
+  love.graphics.draw(ground.img, ground.imgFrame, 0, player.y)
+  
 end
 
 -- The update() method is called every frame
-function basicwalking:update()
+function basicwalking:update(dt)
+  
+   if love.keyboard.isDown('right') then
+		-- This makes sure that the character doesn't go pass the game window's right edge.
+		if player.x < (love.graphics.getWidth() - player.img:getWidth()) then
+			player.x = player.x + (MAX_SPEED * dt)
+		end
+	elseif love.keyboard.isDown('left') then
+		-- This makes sure that the character doesn't go pass the game window's left edge.
+		if player.x > 0 then 
+			player.x = player.x - (MAX_SPEED * dt)
+		end
+	end
   
 end
 
